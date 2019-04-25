71428b2a4e77a1edf58caa9a7a46a3ba735ad3f2
pedro.correa@concrete.com.br <pedro.correa@concrete.com.br>
2019-04-25 15:05:44 -0300

    Initial base commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcoretuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
